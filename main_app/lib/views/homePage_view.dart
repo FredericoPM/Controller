e1fd8d146c78d08e7f3a948d3719dfc2e6af80be
FredericoPM <fredericopradomarques@gmail.com>
2020-12-07 13:23:04 -0300

    home page

--- main_app/lib/views/homePage_view.dart
+++ main_app/lib/views/homePage_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:main_app/widgets/sideMenu_widget.dart';
+
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
 class HomePage extends StatefulWidget {
   @override
   HomePageState createState() => HomePageState();
@@ -20,9 +23,39 @@ class HomePageState extends State<HomePage> {
       home: Scaffold(
           drawer: MenuDrawer(),
           appBar: AppBar(
-            title: Text("Home page"),
+            title: Text("Home"),
             centerTitle: true,
           ),
+        body: FooterView(
+          children:<Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Bem Vindo!", style: TextStyle(fontSize: 40, color: Colors.grey[300],fontWeight: FontWeight.bold)),
+                    SizedBox(height: 12),
+                    Text("Não criou seu primeiro controle ainda?", style: TextStyle(color: Colors.grey)),
+                    SizedBox(height: 5),
+                    Text("Ta esperando o que?", style: TextStyle(color: Colors.grey)),
+                  ],
+                ),
+              ),
+            )
+          ],
+          footer: new Footer(
+            backgroundColor: Color(0xFF2e2e2e),
+            child: Column(
+              children: [
+                Text("APP desenvolvidor por Frederico Prado Marques", style: TextStyle(fontSize: 11, color: Colors.grey)),
+                SizedBox(height: 3),
+                Text("Eng. da Computação PUC Minas", style: TextStyle(fontSize: 11, color: Colors.grey)),
+              ],
+            )
+          ),
+          flex: 1, //default flex is 2
+        ),
         ),
     );
   }
